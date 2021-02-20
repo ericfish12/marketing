4c4f30091b4d30e9f542b8ed85afb62e74b81509
ericfish12 <ericfish1221@gmail.com>
2021-02-19 22:59:50 -0500

    weg

--- Server/db.js
+++ Server/db.js
@@ -1,73 +1,27 @@
-// const Sequelize = require('sequelize')
-
-// const db = new Sequelize (
-//     process.env.DATABASE_URL||`postgres://localhost:5432/PWMESSAGE`,  {
-//         logging: false
-//       }
-// )
-
-
-
-// const Messages = db.define('Messages',{
-//     name:{
-//         type:Sequelize.STRING,
-//         allowNull:false,
-//     },
-//     email:{
-//         type:Sequelize.STRING,
-//         allowNull:false,
-//         validate:{
-//             isEmail:true
-//         },
-      
-//     },
-//     message:{
-//         type:Sequelize.TEXT,
-//         allowNull:false,
-//     }
-// })
-
-
-// async function seed() {
-//     await db.sync({force: true})
-//     console.log('db synced!')
-// }
-
-// //  seed()
-// module.exports = {
-//     db,
-//     Messages,
-//   };
-  
-
-
-
-
-
-
 const Sequelize = require('sequelize')
 
 const db = new Sequelize (
-    process.env.DATABASE_URL||`postgres://localhost:5432/antra_todoList`,  {
+    process.env.DATABASE_URL||`postgres://localhost:5432/PWMESSAGE`,  {
         logging: false
       }
 )
 
 
 
-const TodoLists = db.define('todolists',{
-    userId:{
-        type:Sequelize.INTEGER,
-        
-        defaultValue:1
+const Messages = db.define('Messages',{
+    name:{
+        type:Sequelize.STRING,
+        allowNull:false,
     },
-    completed:{
-        type:Sequelize.BOOLEAN,
-       
-        defaultValue:false
+    email:{
+        type:Sequelize.STRING,
+        allowNull:false,
+        validate:{
+            isEmail:true
+        },
       
     },
-    title:{
+    message:{
         type:Sequelize.TEXT,
         allowNull:false,
     }
@@ -82,6 +36,9 @@ async function seed() {
 //  seed()
 module.exports = {
     db,
-    TodoLists,
+    Messages,
   };
   
+
+
+
