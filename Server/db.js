56d0789d7d6c96da74d6916586ca4e3bf9f3ece3
ericfish12 <ericfish1221@gmail.com>
2021-04-24 15:18:22 -0400

    SSL

--- Server/db.js
+++ Server/db.js
@@ -3,9 +3,16 @@ const Sequelize = require('sequelize')
 const db = new Sequelize (
     process.env.DATABASE_URL||`postgres://localhost:5432/PWMESSAGE`,  {
         logging: false,
-        ssl: { rejectUnauthorized: false } 
+        dialectOptions: {
+            ssl: {      /* <----- Add SSL option */
+              require: true,
+              rejectUnauthorized: false 
+            }
+          },
       }
 )
+
+
 
 
 
