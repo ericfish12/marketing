14446943d9e0c87d29314a0646d31da78891b44b
ericfish12 <ericfish1221@gmail.com>
2021-04-26 20:13:17 -0400

    add data

--- component/Projects.js
+++ component/Projects.js
@@ -11,7 +11,7 @@ export const Projects = () => {
         <div className="ProjectsContainer">
           <div class="GoalHeroLeft">
             <div class="GoalHeroText">
-              <h3> GFI FaxMaker</h3>
+              <h3> GFI FaxMaker<span class = 'float-right'>Jan 2020 - Present</span></h3>
               The GFI FaxMaker Online administration dashboard allows you to
               graphically view your fax communications over time and drill down
               to send or received transmissions. It also gives you a breakdown
@@ -37,7 +37,7 @@ export const Projects = () => {
         <div className="ProjectsContainer">
           <div class="GoalHeroLeft">
             <div class="GoalHeroText">
-              <h3>   Management Dashboard</h3>
+              <h3>   Management Dashboard  <span class = 'float-right'>Aug 2018 - Dec 2019</span></h3>
             
 Insights into quality grades (CISQ, ISO 5055), automated function points, and structural integrity trends to enable objective decision-making, better IT investments, fact-based management of ADM vendors and internal teams. At-a-glance summary of your software health in terms of robustness, security, efficiency, changeability, transferability, and quality. Accurate sizing metrics that show what you have to manage and how far you've gotten. Heatmap to quickly find where you are at the highest risk. Trending analysis to benchmark performance over time. Fast facts on the evolution of your software.
 
@@ -60,7 +60,7 @@ Insights into quality grades (CISQ, ISO 5055), automated function points, and st
         <div className="ProjectsContainer">
           <div class="GoalHeroLeft">
             <div class="GoalHeroText">
-              <h3>    Clarity PPM</h3>
+              <h3>    Clarity PPM <span class = 'float-right'>Aug 2017 - May 2018</span> </h3>
            
 Clarity PPM enables teams to communicate business outcomes across the organization, allowing stakeholders to agree on a common set of objectives. Automatically update project financials with the actual time spent on each task. Managers can see teams' start and finish dates, booking status, estimated time of completion, total allocations, and total actuals. Create a visual task list on the Clarity PPM (formerly CA Project Portfolio Management), task boards. It provides users with their process flow, assigns staff to tasks, and prioritizes work. Keep all team members in sync and share plans with other groups and executives. Clarity PPM’s financial planning pages have been redesigned to mimic Microsoft Excel spreadsheets in both looks and feel. It provides one consolidated view of each employee’s project queue and their schedule. Managers explore staffing options with telescoping and pinning features that replace outdated filtering and matching tools.
 
@@ -80,7 +80,7 @@ Clarity PPM enables teams to communicate business outcomes across the organizati
         <div className="ProjectsContainer">
           <div class="GoalHeroLeft">
             <div class="GoalHeroText">
-              <h3>       Inova for Pharma</h3>
+              <h3>       Inova for Pharma <span class = 'float-right'>Aug 2016 - May 2017</span> </h3>
            
           
 Inova is the partnering platform for pharma companies to manage their opportunities more efficiently. They find all their partnering information in one place, track their deals and alliances easily and report on their pipeline and activities It has several features like Search Evaluation, Opportunities Tracking, Alliance Management, Reporting.
